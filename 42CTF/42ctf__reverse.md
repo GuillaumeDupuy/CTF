86e9dd4b14b3dc05ef1c495b2b755312d0f1f709
Guillaume <guillaume.dupuy@ynov.com>
2022-10-16 02:58:04 +0200

    add new flag && update scripts sh

--- 42CTF/42ctf__reverse.md
+++ 42CTF/42ctf__reverse.md
@@ -7,3 +7,15 @@ strings _
 flag : ``
 Was_really_easy
 ``
+
+# Come on!
+
+In Progress
+
+# IN STACK WE TRUST
+
+Decompile teh binary and check FUN_080491b4
+
+flag : ``
+1ts_t00_34sy_4_y0u
+``
--- 42CTF/scripts/find_rsa.sh
+++ 42CTF/scripts/find_rsa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-openssl rsa -noout -text -inform PEM -in pub.pem -pubin > pubkey
-cat pubkey | sed -e 's/^Pub.*//' -e 's/^Mod.*//' -e 's/Exp.*//' -e 's/://g' -e 's/ //g' > temp
-tr -d '\n' < temp > N
-cat pubkey && echo ""
+openssl rsa -noout -text -inform PEM -in key_pub1.pem -pubin > pubkey.txt
+cat pubkey.txt | sed -e 's/^Pub.*//' -e 's/^Mod.*//' -e 's/Exp.*//' -e 's/://g' -e 's/ //g' > temp
+tr -d '\n' < temp > N.txt
+cat pubkey.txt > pubkey.txt
 rm temp
 
 # public key value ==> N
-cat N | xargs python -c "import sys; print(int(str(sys.argv[1]), 16))" > N.txt
+cat N.txt | xargs python -c "import sys; print(int(str(sys.argv[1]), 16))" > N.txt
 
 # ciphertext value ==> c
-cat ciphertext | xargs python -c "import sys; print(int(str(sys.argv[1]), 16))" > C.txt
+cat message1.txt | xargs python -c "import sys; print(int(str(sys.argv[1]), 16))" > C.txt
