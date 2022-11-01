986b3514473790a63c257f26118d9cceaf70ab42
Guillaume <guillaume.dupuy@ynov.com>
2022-11-01 22:05:08 +0100

    update api.py

--- Stats/api.py
+++ Stats/api.py
@@ -40,7 +40,7 @@ if resp.status_code != 200:
 # Get the data
 data = resp.json()
 
-info = ["```text\n","\n"]
+info = ["``text\n"]
 
 info.append("🧑‍💻 Name: "+data["nom"]+"\n")
 
@@ -50,7 +50,7 @@ info.append("🥇 Ranking: "+str(data["position"])+"\n")
 
 info.append("✅ Number of Challenges Finish: "+str(len(data["validations"]))+"\n")
 
-info.append("```\n")
+info.append("``\n")
 
 if conttemp == info:
     print("No change in README.md")
