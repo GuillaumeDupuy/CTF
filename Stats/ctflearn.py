428fb986e3f20daf16c52da35ca48b1c5a472b26
Guillaume <guillaume.dupuy@ynov.com>
2022-11-12 17:15:43 +0100

    fix scrap stats ctflearn

--- Stats/ctflearn.py
+++ Stats/ctflearn.py
@@ -2,29 +2,29 @@ from bs4 import BeautifulSoup
 import requests 
 import os 
 
-DEL_START  ="<!--CTFLEARN-->"
-DEL_END    ="<!--/CTFLEARN-->"
+# DEL_START  ="<!--CTFLEARN-->"
+# DEL_END    ="<!--/CTFLEARN-->"
 
-n = 0
-readmefile=open('README.md','r')
-lines = readmefile.readlines()
-readmefile.close()
+# n = 0
+# readmefile=open('README.md','r')
+# lines = readmefile.readlines()
+# readmefile.close()
 
-start =-1
-end = -1
-for line in lines:
-    if DEL_START in line:
-        start = n
-    if DEL_END in line:
-        end = n
-    n+=1
-if start == -1 or end == -1:
-    print("Error: Delimiter not found")
-    exit(1)
+# start =-1
+# end = -1
+# for line in lines:
+#     if DEL_START in line:
+#         start = n
+#     if DEL_END in line:
+#         end = n
+#     n+=1
+# if start == -1 or end == -1:
+#     print("Error: Delimiter not found")
+#     exit(1)
 
-partOne = lines[:start+1]
-conttemp = lines[start+1:end]
-partTwo = lines[end:]
+# partOne = lines[:start+1]
+# conttemp = lines[start+1:end]
+# partTwo = lines[end:]
 
 url="https://ctflearn.com/user/Varius93"
 response = requests.get(url)
@@ -36,7 +36,7 @@ name = soup.find("h5", {"class": "text-left font-weight-bold font-primary my-aut
 # Get the number of rank
 rank = soup.find("span", {"class": "font-primary mt-2"}).text.strip()
 
-rank = rank.split("th")[0]
+rank = rank.split("nd")[0]
 
 # Get the number of points
 points = soup.find("div", {"class": "stats mt-3"}).text.strip()
@@ -51,17 +51,19 @@ data.append("🥇 Ranking: "+rank+"\n")
 data.append("✅ Number of Challenges Finish: 125\n")
 data.append("```\n")
 
-if conttemp == data:
-    print("No change in README.md")
-    exit(0)
+print(data)
 
-result = partOne + data + partTwo
-readmefile=open('README.md','w')
-readmefile.writelines(result)
-readmefile.close()
+# if conttemp == data:
+#     print("No change in README.md")
+#     exit(0)
 
-os.system('git config --local user.email "github-actions[bot]@users.noreply.github.com"')
-os.system('git config --local user.name "github-actions[bot]"')
-os.system('git add .')
-os.system('git commit -m "CTFLearn Stats Update"')
-os.system('git push')
+# result = partOne + data + partTwo
+# readmefile=open('README.md','w')
+# readmefile.writelines(result)
+# readmefile.close()
+
+# os.system('git config --local user.email "github-actions[bot]@users.noreply.github.com"')
+# os.system('git config --local user.name "github-actions[bot]"')
+# os.system('git add .')
+# os.system('git commit -m "CTFLearn Stats Update"')
+# os.system('git push')
